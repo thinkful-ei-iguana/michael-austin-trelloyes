f5a34136b636e3952aa5afa1e6ceacb5724e7282
Michael Weedman <michael.weedman@gmail.com>
2019-11-11 12:30:03 -0700

    Tooltips and split set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './App.css';
+import Split from './composition/split';
+import Tooltip from './composition/tooltip';
+
+// after
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Split className='left' flexBasis='2'>
+        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
+        {secondTooltip}
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
     </main>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import STORE from './store';
+
+
+// RENDERS MARKUP MATCHING DESIGN HTML FOR CARDS //
